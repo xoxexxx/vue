b9d5fa43c2356fe2a3fa27c38a2f2d8e5a2e8006
I B <ya@MacBook-Air-I.local>
2024-05-19 11:01:26 +0300

    vue+tailwind conf

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container mx-auto px-10">
+  <header>
+    <div>
+      <nav class="flex gap-10 my-5">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+  <main>HOME</main>
+</template>s
